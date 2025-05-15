c48b3ef1495591198a4be559a73995c4aa49c098
Valen-0612 <vgr230115@gmail.com>
2025-05-15 20:45:19 +0000

    generador de cartas aleatorias no logre que el boton funcionara

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function generarCarta(){
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+}
+
+
+
+
+
+window.onload = function () {
+    const suits = ["♦", "♥", "♠", "♣"];
+    const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+
+    document.getElementById("top-suit").textContent = randomSuit;
+    document.getElementById("bottom-suit").textContent = randomSuit;
+    document.getElementById("number").textContent = randomValue;
+
+    const color = (randomSuit === "♥" || randomSuit === "♦") ? "red" : "black";
+
+    document.getElementById("top-suit").style.color = color;
+    document.getElementById("bottom-suit").style.color = color;
+    document.getElementById("number").style.color = color;
+
+   
 };
+
